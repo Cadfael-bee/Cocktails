f395eb91e01dcc320a35614f6fdee26b6714308a
Elii Povolotskyi <Elii_Povolotskyi@epam.com>
2020-03-05 13:33:00 +0200

    commit

--- src/main/java/com/my/store/model/User.java
+++ src/main/java/com/my/store/model/User.java
@@ -1,22 +1,15 @@
 package com.my.store.model;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 
 @AllArgsConstructor
-@RequiredArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
@@ -24,28 +17,18 @@ import lombok.RequiredArgsConstructor;
 public class User {
 
 	@Id
-	@NonNull
 	private Integer id;
 
-	@NonNull
 	private String userName;
 
-	@NonNull
 	private String fName;
 
-	@NonNull
 	private String lName;
 
-	@NonNull
 	private String email;
 
-	@NonNull
 	private String password;
 
-	@NonNull
 	private Boolean news;
-	
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<Order> orders = new ArrayList<>();
-	
+
 }
